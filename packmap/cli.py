41db64a0d25384beae4688a291e9d4df0aab9428
John Vrbanac <john.vrbanac@linux.com>
2014-08-17 19:21:50 -0500

    Adding pdf graph output of results

--- packmap/cli.py
+++ packmap/cli.py
@@ -16,6 +16,7 @@ limitations under the License.
 import argparse
 import subprocess
 import json
+import pydot
 
 from packmap import env
 from packmap import finder
@@ -48,6 +49,7 @@ class PackMapClient(object):
         parser.add_argument('--install-path', type=str)
         parser.add_argument('--keep-env', action='store_true')
         parser.add_argument('--no-json-results', action='store_true')
+        parser.add_argument('--pdf-results', action='store_true')
 
         return parser
 
@@ -65,6 +67,29 @@ class PackMapClient(object):
     def write_results_to_file(self, results, filename):
         json_file = open(filename, 'w')
         json_file.write(json.dumps(results))
+
+    def create_results_pdf(self, results, filename):
+        graph_items = {}
+        graph = pydot.Dot(graph_type='digraph', splines='spline')
+
+        # Create Package Nodes
+        for req_name, req in results.items():
+            node_name = '{0}\n{1}'.format(req_name, req.get('version'))
+            node = pydot.Node(node_name, style="filled", fillcolor='#cccccc')
+            graph.add_node(node)
+            graph_items[req_name] = node
+
+        # Create Relationships
+        for req_name, req in results.items():
+            main_node = graph_items.get(req_name)
+
+            for sub_req in req.get('requirements'):
+                sub_node = graph_items.get(sub_req.get('name'))
+                edge = pydot.Edge(main_node, sub_node, arrowhead='vee')
+                graph.add_edge(edge)
+
+        print('Saving PDF Graph: {0}'.format(filename))
+        graph.write(filename, format='pdf')
 
     def run(self):
         args = self.parse_args()
@@ -84,8 +109,12 @@ class PackMapClient(object):
         self.results = self.execute_finder(manager, args.package_name)
 
         if not args.no_json_results:
-            filename = './{pkg_name}.json'.format(pkg_name=args.package_name)
+            filename = './{name}_deps.json'.format(name=args.package_name)
             self.write_results_to_file(self.results, filename)
+
+        if args.pdf_results:
+            filename = './{name}_graph.pdf'.format(name=args.package_name)
+            self.create_results_pdf(self.results, filename)
 
         if not args.keep_env:
             manager.clean_up()
